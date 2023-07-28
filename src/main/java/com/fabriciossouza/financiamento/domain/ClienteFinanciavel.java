6f70dc28b40a81a1fbe36fc6dd87a9b31fb52a69
Fabricio <fsouza@Novo.lan>
2023-07-28 11:00:08 +0100

    fix: definindo valores default para financiamento

--- src/main/java/com/fabriciossouza/financiamento/domain/ClienteFinanciavel.java
+++ src/main/java/com/fabriciossouza/financiamento/domain/ClienteFinanciavel.java
@@ -2,12 +2,23 @@ package com.fabriciossouza.financiamento.domain;
 
 public interface ClienteFinanciavel {
 
+    int EMPRESA_VALOR_SIMPLES_NACIONAL = 1_000_000;
+    int EMPRESA_VALOR_MEI = 100_000;
+
     double calcularLimiteAprovado();
 
     default double calcularJuros(double valorSolicitado) {
-        if (valorSolicitado <= 100_000) return 1.0;
-        if (valorSolicitado <= 1_000_000) return 1.5;
+        if (isFinanciamentoMedioValor(valorSolicitado)) return 1.0;
+        if (isFinanciamentoGrandeValor(valorSolicitado)) return 1.5;
         return 2.0;
+    }
+
+    private boolean isFinanciamentoGrandeValor(double valorSolicitado) {
+        return valorSolicitado <= EMPRESA_VALOR_SIMPLES_NACIONAL;
+    }
+
+    private boolean isFinanciamentoMedioValor(double valorSolicitado) {
+        return valorSolicitado <= EMPRESA_VALOR_MEI;
     }
 
 }
