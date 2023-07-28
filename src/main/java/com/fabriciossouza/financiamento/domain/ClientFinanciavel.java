13faa9e69fb6865369b77cee7624eea2bd1fd3b7
Fabricio <fsouza@Novo.lan>
2023-07-28 10:46:04 +0100

    inclusao do calculo de juros

--- src/main/java/com/fabriciossouza/financiamento/domain/ClientFinanciavel.java
+++ src/main/java/com/fabriciossouza/financiamento/domain/ClientFinanciavel.java
@@ -4,4 +4,10 @@ public interface ClientFinanciavel {
 
     double calcularLimiteAprovado();
 
+    default double calcularJuros(double valorSolicitado) {
+        if (valorSolicitado <= 100_000) return 1.0;
+        if (valorSolicitado <= 1_000_000) return 1.5;
+        return 2.0;
+    }
+
 }
--- src/main/java/com/fabriciossouza/financiamento/service/ServicoFinanciamento.java
+++ src/main/java/com/fabriciossouza/financiamento/service/ServicoFinanciamento.java
@@ -2,16 +2,22 @@ package com.fabriciossouza.financiamento.service;
 
 import com.fabriciossouza.financiamento.domain.ClientFinanciavel;
 
+import static java.lang.System.out;
+
+
 public class ServicoFinanciamento {
+
+    private static final String LOG_FINANCIAMENTO_APROVADO = "Financiamento aprovado no valor de R$%.2f com juros de %.2f%%. Limite máximo de %.2f%n";
 
     public void solicitarFinanciamento(ClientFinanciavel clientFinanciavel, double valorSolicitado) {
         double limiteAprovado = clientFinanciavel.calcularLimiteAprovado();
+        double juros = clientFinanciavel.calcularJuros(valorSolicitado);
 
         if (limiteAprovado < valorSolicitado) {
             throw new RuntimeException(String.format("Financiamento não aprovado. Limite máximo de %.2f", limiteAprovado));
         }
 
-        System.out.printf("DEBUG: Financiamento aprovado. Limite máximo de %.2f%n", limiteAprovado);
+        out.printf("DEBUG: ".concat(LOG_FINANCIAMENTO_APROVADO), valorSolicitado, juros, limiteAprovado);;
     }
 
     public double consultarLimiteAprovado(ClientFinanciavel clientFinanciavel) {
